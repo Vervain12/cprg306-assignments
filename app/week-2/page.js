91a85602bff77079ddbd2b1918f29d583ccb6633
Jeremy Cole <jeremycole059@gmail.com>
2025-01-21 15:50:14 -0700

    Added Week-2 Assignment

---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import StudenInfo from "./student-info";
+
+export default function Page() {
+    return (
+      <main>
+        <h1>Shopping List</h1>
+        <StudenInfo />
+      </main>
+    );
+  }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function StudenInfo() {
+    return(
+        <div>
+            <p>Jeremy Cole</p>
+            <Link href="https://github.com/Vervain12/cprg306-assignments.git">Github Repo</Link>
+        </div>
+    )
+}
