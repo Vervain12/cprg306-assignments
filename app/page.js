7597db0f4d09be8d13402342fc025c9a157850d4
Jeremy Cole <jeremycole059@gmail.com>
2025-02-12 12:37:49 -0700

    Week 6 Complete

--- app/page.js
+++ app/page.js
@@ -16,6 +16,9 @@ export default function Page(){
       <div>
         <Link href="week-5">Week 5 Assignment</Link>
       </div>
+      <div>
+        <Link href="week-6">Week 6 Assignment</Link>
+      </div>
     </main>
   )
 }
---
+++ app/week-6/item-list.js
@@ -0,0 +1,49 @@
+"use client"
+
+import React, { useState } from "react";
+import Item from "./item";
+import items from "./items.json";
+
+export default function ItemList (){
+
+    const [sortBy, setSortBy] = useState("name");
+
+    const sortByName = () => {
+        setSortBy("name")
+    }
+    const sortByCategory = () => {
+        setSortBy("category")
+    }
+    
+    const sortedItems =[...items].sort((a, b) => {
+        if (sortBy === "name") {
+            return a.name.localeCompare(b.name);
+        }
+        else if (sortBy === "category") {
+            return a.category.localeCompare(b.category);
+        }
+    });
+
+    let itemList = sortedItems.map(item => 
+        <Item
+            id={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            category={item.category}
+        />);
+
+    return (
+        <div>
+            <div className="flex items-center gap-4 m-4 max-w-sm">
+                <text className="text-pink-300">Sort By:</text>
+                <button onClick={sortByName} type="button"
+                    className={`px-4 py-1 text-black text-sm font-semibold transition-colors ${sortBy === "name" ? 'bg-pink-300' : 'bg-gray-300'}`}>Name</button>
+                <button onClick={sortByCategory} type="button"
+                    className={`px-4 py-1 text-black text-sm font-semibold transition-colors ${sortBy === "category" ? 'bg-pink-300' : 'bg-gray-300'}`}>Category</button>
+            </div>
+            <ol>
+                {itemList}
+            </ol>
+        </div>
+    )
+}
